96d3ec21aef8102d1fff3e8a2562ac85575a154a
Nicolas <nicolas.gehin@pixelea.fr>
2024-01-16 16:11:24 +0100

    add block_styles

---
+++ block-styles/core-list/with-arrows/register.php
@@ -0,0 +1,19 @@
+<?php
+    if ( function_exists( 'register_block_style' ) ) {
+    register_block_style(
+        'core/list',
+        array(
+            'name'         => 'arrows',
+            'label'        => __( 'avec flèches', 'ng1' ),
+            'is_default'   => false,
+            'style_handle' => 'core-list-arrow'
+        )
+    );
+    wp_enqueue_style(
+        'core-list-arrow', // Même handle que vous avez spécifié ci-dessus
+        get_stylesheet_directory_uri() . '/block-styles/core-list/with-arrows/style.css', // Chemin absolu vers votre fichier CSS
+        array(), // Dépendances (peut être vide si vous n'avez pas de dépendances)
+        '', // Version du fichier (peut être une chaîne vide)
+        'all' // Type de média (peut être 'all', 'screen', 'print', etc.)
+    );
+}
